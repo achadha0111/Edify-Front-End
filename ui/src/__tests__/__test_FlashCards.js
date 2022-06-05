3d40bc3a68883595fc0323240d4124d9fd522748
Aayush Chadha <aayush.chadha@student.manchester.ac.uk>
2022-06-05 17:33:32 +0100

    Split test files to aid readability

--- ui/src/__tests__/__test_FlashCards.js
+++ ui/src/__tests__/__test_FlashCards.js
@@ -1,48 +1,7 @@
-import NewNote from "../pages/NewNote"
 import {render, screen} from "../setupTests";
+import NewNote from "../pages/NewNote";
 import userEvent from "@testing-library/user-event";
-import {queryByRole, waitForElementToBeRemoved, within} from "@testing-library/react";
-
-/* TODO Tests to Add:
-*   1. Test adding blocks between blocks
-*   2. DONE flashcard being added to new deck
-*   3. DONE flashcard being added to existing deck
-*   4. DONE Test flashcard being edited
-*   5. DONE flashcard delete
-*   6. DONE deck delete
-*   7. Test block delete  */
-
-test("displaying startup page with initial block", () => {
-    render(<NewNote/>);
-
-    const cells = screen.getAllByRole('cell', {name:/RichText/i});
-
-    expect(cells.length).toBe(1);
-});
-
-test("clicking text button adds a text block", async () => {
-    render(<NewNote/>);
-
-    const textBoxButton = screen.getByLabelText("Add text block");
-
-    await userEvent.click(textBoxButton);
-
-    const cells = screen.getAllByRole('cell', {name:/RichText/i});
-
-    expect(cells.length).toBe(2);
-});
-
-test("clicking code button adds a code cell", async () => {
-    render(<NewNote/>);
-
-    const codeBlockButton = screen.getByLabelText("Insert code block");
-
-    await userEvent.click(codeBlockButton);
-
-    const codeCell = screen.getAllByRole('cell', {name:/Code/i});
-
-    expect(codeCell.length).toBe(1);
-});
+import {waitForElementToBeRemoved} from "@testing-library/react";
 
 test("add and edit flashcard", async () => {
     render(<NewNote/>);

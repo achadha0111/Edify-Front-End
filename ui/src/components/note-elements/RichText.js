e9bc73dfe35b6595bed6069204512c50369a2ec9
Aayush Chadha <aayush.chadha@student.manchester.ac.uk>
2022-07-08 00:00:28 +0100

    Add a custom handler for equations

--- ui/src/components/note-elements/RichText.js
+++ ui/src/components/note-elements/RichText.js
@@ -1,10 +1,36 @@
 import React, {useEffect, useState} from "react";
-import ReactQuill from 'react-quill';
+import ReactQuill, {Quill} from 'react-quill';
 import '../styles/EditorStyles.css';
 import 'katex/dist/katex.min.css';
 import katex from 'katex';
 
 window.katex = katex;
+
+const SnowTheme = Quill.import("themes/snow");
+
+class ExtendSnowTheme extends SnowTheme {
+    constructor(quill, options) {
+        super(quill, options);
+
+        quill.on("selection-change", (range) => {
+            if (range) {
+                quill.theme.tooltip.show();
+                quill.theme.tooltip.position(quill.getBounds(range));
+            }
+        });
+    }
+}
+
+// Quill.register("themes/snow", ExtendSnowTheme);
+
+function handleFormula() {
+    let tooltip = this.quill.theme.tooltip;
+    tooltip.edit('formula', 'myformula');
+
+    let range = this.quill.getSelection();
+    let preview = this.quill.getContents(range.index, range.length);
+    console.log(preview);
+}
 
 export default function RichText(props) {
     const [value, setValue] = useState(props.data);
@@ -31,21 +57,28 @@ export default function RichText(props) {
     />
     }
 
+// function formulaHandler() {
+//    this.quill
+// }
+
 RichText.modules = {
-    toolbar: [
-        [{ 'header': '1'}, {'header': '2'}, { 'font': [] }],
+    toolbar: {
+        container: [[{ 'header': '1'}, {'header': '2'}, { 'font': [] }],
         [{size: []}],
         ['bold', 'italic', 'underline', 'strike', 'blockquote', 'code'],
         [{'list': 'ordered'}, {'list': 'bullet'},
             {'indent': '-1'}, {'indent': '+1'}],
         ['link', 'image', 'video'],
         ['clean'],
-        ['formula']
-    ],
+        ['formula']],
+        handlers: {
+            formula: handleFormula
+        }
+    },
     clipboard: {
         // toggle to add extra line breaks when pasting HTML:
         matchVisual: false,
-    }
+    },
 }
 
 RichText.defaultProps = {

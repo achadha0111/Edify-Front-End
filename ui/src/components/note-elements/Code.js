c43140dafbe5a61015d3bfa105936a76369f573d
Aayush Chadha <aayush.chadha@student.manchester.ac.uk>
2022-07-09 00:32:28 +0100

    Update code blocks to render data passed to them

--- ui/src/components/note-elements/Code.js
+++ ui/src/components/note-elements/Code.js
@@ -4,13 +4,12 @@ import {useEffect, useState} from "react";
 
 export default function Code(props) {
 
-    const [userCode, setUserCode] = useState("");
-    const [previousKey, setPreviousKey] = useState("");
+    const [userCode, setUserCode] = useState(props.data);
 
     return (
             <CodeMirror
                 className="Code"
-                value=""
+                value={userCode}
                 extensions={[python()]}
                 onFocus={props.onFocus}
                 onChange={(value, viewUpdate) => {

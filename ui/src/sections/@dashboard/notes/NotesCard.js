36833508b39436b5bce0af55aa3e644658ded5ad
Aayush Chadha <aayush.chadha@student.manchester.ac.uk>
2022-07-25 21:20:20 +0100

    Add delete note functionality

--- ui/src/sections/@dashboard/notes/NotesCard.js
+++ ui/src/sections/@dashboard/notes/NotesCard.js
@@ -1,10 +1,13 @@
 import PropTypes from 'prop-types';
 import { Link as RouterLink } from 'react-router-dom';
 // material
-import {styled } from '@mui/material/styles';
-import { Card, Grid, Typography, CardContent } from '@mui/material';
+import {styled} from '@mui/material/styles';
+import {Card, Grid, Typography, CardContent, Tooltip} from '@mui/material';
 // utils
 import { fDate } from '../../../utils/formatTime';
+import Button from "@mui/material/Button";
+import CardActions from "@mui/material/CardActions";
+import DeleteIcon from "@mui/icons-material/Delete";
 
 // ----------------------------------------------------------------------
 
@@ -17,6 +20,15 @@ const TitleStyle = styled(RouterLink)({
   textDecoration: "none",
 });
 
+const CardActionsStyle = styled(CardActions)({
+    display: 'flex',
+    alignItems: 'flex-end',
+});
+
+const StyledButton = styled(Button)({
+    marginLeft: 'auto'
+})
+
 // ----------------------------------------------------------------------
 
 NotesCard.propTypes = {
@@ -24,10 +36,14 @@ NotesCard.propTypes = {
   index: PropTypes.number,
 };
 
-export default function NotesCard({ note, index }) {
+export default function NotesCard({ note, index, deleteNote }) {
   const { noteName, lastSaved, id } = note;
   const latestPostLarge = index === 0;
   const latestPost = index === 1 || index === 2;
+
+  const triggerDelete = () => {
+      deleteNote(id);
+  }
 
   return (
       <Grid item xs={12} sm={latestPostLarge ? 8 : 6} md={latestPostLarge ? 4 : 3}>
@@ -54,6 +70,12 @@ export default function NotesCard({ note, index }) {
               {noteName}
             </TitleStyle>
           </CardContent>
+          <CardActionsStyle className="NotesCardAction">
+              <Tooltip title="Delete note">
+                  <StyledButton size="small" aria-label="DeleteNote" onClick={triggerDelete}><DeleteIcon/></StyledButton>
+              </Tooltip>
+
+          </CardActionsStyle>
         </Card>
       </Grid>
   );
